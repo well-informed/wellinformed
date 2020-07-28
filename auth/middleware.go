92773b223f6ec44d4970cb0c03a2018f342055e1
Joshua ledbetter <ledbetterljoshua@gmail.com>
2020-07-27 21:30:28 -0500

    fix refresh token

--- auth/middleware.go
+++ auth/middleware.go
@@ -155,7 +155,12 @@ func RefreshToken(db wellinformed.Persistor) http.HandlerFunc {
 			return
 		}
 
-		user, err := db.GetUserById(claims["jti"].(int64))
+		id, err := strconv.ParseInt(claims["jti"].(string), 10, 64)
+		if err != nil {
+			log.Error("could not parse claim into int. err: ", err)
+			return
+		}
+		user, err := db.GetUserById(id)
 
 		log.Printf("user: %v", user)
 
